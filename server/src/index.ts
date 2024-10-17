3911b44ed21d243522372aaed63cc92012633cfb
Jeffrey Chu <jeffcolinchu@gmail.com>
2024-10-16 22:27:26 -0400

    setup initial node implementation and other configs

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,3 +3,15 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 
 dotenv.config()
+
+const app: Application = express()
+const PORT = process.env.PORT || 8000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server is running...')
+})
+
+app.listen(PORT, () => console.log(`Server is live on http://localhost:${PORT}`))
